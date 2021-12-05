5b11f3c1dd88b4efd849c1278122f303093cfd13
Windrushfarer <windrushfarer@gmail.com>
2021-12-05 22:43:45 +0100

    added day 4 part two solution

--- packages/day_4/src/main.rs
+++ packages/day_4/src/main.rs
@@ -105,7 +105,108 @@ fn part_one() {
   println!("{:?}", score)
 }
 
+fn part_two() {
+  let (nums, boards) = include_str!("./input.txt")
+    .split_once("\n\n")
+    .unwrap();
+
+  let nums: Vec<u32> = nums.split(",")
+    .map(|n| n.parse().unwrap())
+    .collect();
+
+  let mut boards: Vec<Vec<Vec<u32>>> = boards.split("\n\n")
+    .map(|board| {
+      board
+        .split("\n")
+        .map(|r| {
+          r.split_whitespace()
+            .filter(|x| !x.is_empty())
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<_>>()
+        })
+        .collect()
+    }).collect::<Vec<_>>();
+
+  let mut winner_found = false;
+  let mut first_winner: i8 = -1;
+
+  let mut visited: Vec<Vec<Vec<u8>>> = (0..boards.len())
+    .map(|_| {
+      let checks: Vec<u8> = vec![0; SIZE];
+
+      return vec![checks.clone(); SIZE];
+    })
+    .collect::<Vec<_>>();
+
+  let score = nums.iter()
+    .map_while(|num| {
+      if winner_found {
+        return None;
+      }
+
+      boards.iter()
+        .enumerate()
+        .for_each(|(i, board) | {
+          board.iter()
+            .enumerate()
+            .for_each(|(r, row)| {
+              row.iter()
+                .enumerate()
+                .for_each(|(c, col)| {
+                  if *col == *num {
+                    visited[i][r][c] = 1 as u8;
+                  }
+                })
+            })
+        });
+
+
+      let winner = boards.iter()
+        .enumerate()
+        .filter(|(i, board)| {
+          first_winner != *i as i8 && has_bingo(&visited.get(*i).unwrap())
+        })
+        .last();
+
+      if winner.is_some() {
+        let (board_index, winner_board) = winner.unwrap();
+
+        if first_winner == -1 {
+          first_winner = board_index as i8;
+
+          return Some(0);
+        }
+        println!("{}", first_winner);
+        let score_unvisited: u32 = winner_board.iter()
+          .enumerate()
+          .fold(0, |sum, (r_index, row)| {
+            let row_sum: u32 = row.iter()
+              .enumerate()
+              .fold(0, |s, (col_index, n)| {
+                if visited[board_index][r_index][col_index] == 0 {
+                  return s + n
+                }
+
+                return s
+              });
+
+            sum + row_sum as u32
+          });
+
+        winner_found = true;
+
+        return Some(score_unvisited * *num)
+      }
+
+      return Some(0);
+    })
+    .last()
+    .unwrap();
+
+  println!("{:?}", score)
+}
 
 pub fn main() {
-  part_one()
+  part_one();
+  part_two();
 }
