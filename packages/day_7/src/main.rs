197653ab8a447118d1f1cfc0c87086a8fa746cd3
Windrushfarer <windrushfarer@gmail.com>
2021-12-08 22:48:10 +0100

    day 7 part two

--- packages/day_7/src/main.rs
+++ packages/day_7/src/main.rs
@@ -9,10 +9,36 @@ fn part_one() {
 
   println!(
     "{}",
+    // Median sum
     subs.iter().map(|n| (n - median_value).abs()).sum::<i32>()
   );
 }
 
+fn part_two() {
+  let nums = include_str!("./input.txt")
+    .split(',')
+    .map(|n| n.parse().unwrap())
+    .collect::<Vec<i32>>();
+  let max = nums.iter().max().unwrap();
+
+  let min = (0..*max)
+    .map(|step| {
+      nums
+        .iter()
+        .map(|n| {
+          let distance = (n - step).abs();
+
+          distance * (distance + 1) / 2
+        })
+        .sum::<i32>()
+    })
+    .min()
+    .unwrap();
+
+  println!("{}", min);
+}
+
 pub fn main() {
-  part_one()
+  part_one();
+  part_two();
 }
